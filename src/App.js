91c122bbab91640d213022e58119692869ea1c86
Tuğrul Yüce <tugrulyuce80@gmail.com>
2024-01-08 20:13:12 +0300

    singlepostpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./components/topBar/TopBar";
 import HomePage from "./pages/homepage/HomePage";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
     <TopBar/>
-    <HomePage/>
+    <Single/>
     </>
   );
 }
